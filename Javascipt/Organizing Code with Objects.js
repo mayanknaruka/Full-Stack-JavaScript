96f1eaea0a61b0489f43868e72276447381fb344
mayanknaruka <mayanknaruka47@gmail.com>
2026-06-20 22:11:13 +0530

    ads

--- Javascipt/Organizing Code with Objects.js
+++ Javascipt/Organizing Code with Objects.js
@@ -12,3 +12,12 @@ console.log(myObject.property); // "Value!"
 
 // bracket notation
 console.log(myObject["obnoxious property"]); // [Function]
+
+
+const variable = "property";
+
+// "undefined" because it's looking for a property named "variable" in our object
+console.log(myObject.variable);
+
+// this is equivalent to myObject["property"] and returns "Value!"
+console.log(myObject[variable]);
