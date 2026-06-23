7fe15b38c2ee6461f9757fe14778b2a25a8bd20d
mayanknaruka <mayanknaruka47@gmail.com>
2026-06-23 22:30:26 +0530

    objects

--- Javascipt/Object Constructors.js
+++ Javascipt/Object Constructors.js
@@ -2,3 +2,26 @@ function Player(name, marker) {
   this.name = name;
   this.marker = marker;
 }
+
+const player = new Player("steve", "X");
+console.log(player.name); // "steve"
+
+
+let animal = {
+  eats: true
+};
+
+let rabbit = {
+  jumps: true,
+  __proto__: animal
+};
+
+for(let prop in rabbit) {
+  let isOwn = rabbit.hasOwnProperty(prop);
+
+  if (isOwn) {
+    alert(`Our: ${prop}`); // Our: jumps
+  } else {
+    alert(`Inherited: ${prop}`); // Inherited: eats
+  }
+}
