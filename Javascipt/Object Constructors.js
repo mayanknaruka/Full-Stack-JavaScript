9e95a6d0c3a985f3e05215d00262d4bd4e8c6931
mayanknaruka <mayanknaruka47@gmail.com>
2026-06-24 13:46:26 +0530

    solve question 5 of js.info

--- Javascipt/Object Constructors.js
+++ Javascipt/Object Constructors.js
@@ -60,3 +60,30 @@ player2.sayName(); // Hello, I'm also steve!
 
 player1.getMarker(); // My marker is "X"
 player2.getMarker(); // My marker is "O"
+
+
+// question 5 problem of js.info
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach.push(food);
+  }
+};
+
+let speedy = {
+  __proto__: hamster,
+  stomach: []
+};
+
+let lazy = {
+  __proto__: hamster,
+  stomach: []
+};
+
+// Speedy one found the food
+speedy.eat("apple");
+alert( speedy.stomach ); // apple
+
+// Lazy one's stomach is empty
+alert( lazy.stomach ); // <nothing>
