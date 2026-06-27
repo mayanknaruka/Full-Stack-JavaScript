6289d8a94af33b8b9af321380f3a0fee62f094d5
mayanknaruka <mayanknaruka47@gmail.com>
2026-06-27 21:04:00 +0530

    closure

--- Javascipt/03-Factory Functions.js
+++ Javascipt/03-Factory Functions.js
@@ -43,3 +43,34 @@ console.log({
   discordName: josh.discordName,
   reputation: josh.getReputation()
 });
+
+
+// calculator
+const calculator = (() => {
+  let lastResult;
+
+  const add = (a, b) => {
+    lastResult = a + b;
+    return lastResult;
+  };
+  const subtract = (a, b) => {
+    lastResult = a - b;
+    return lastResult;
+  };
+  const multiply = (a, b) => {
+    lastResult = a * b;
+    return lastResult;
+  };
+  const divide = (a, b) => {
+    lastResult = a / b;
+    return lastResult;
+  };
+  const getLastResult = () => lastResult;
+
+  return { add, subtract, multiply, divide, getLastResult };
+})();
+
+console.log(calculator.add(3, 5)); // 8
+console.log(calculator.subtract(6, 2)); // 4
+console.log(calculator.getLastResult()); // 4
+console.log(calculator.multiply(14, 5534)); // 77476
