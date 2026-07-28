cff30079dd4424dfa060cbfb1477e3626315b6fd
mayanknaruka <mayanknaruka47@gmail.com>
2026-07-28 21:11:29 +0530

    ..

--- Javascipt/04-Classes.js
+++ Javascipt/04-Classes.js
@@ -22,3 +22,17 @@ const Rectangle = class Rectangle2 {
   }
 };
 
+class ClassWithStaticMethod {
+  static staticProperty = "someValue";
+  static staticMethod() {
+    return "static method has been called.";
+  }
+  static {
+    console.log("Class static initialization block called");
+  }
+}
+
+console.log(ClassWithStaticMethod.staticProperty);
+// Expected output: "someValue"
+console.log(ClassWithStaticMethod.staticMethod());
+// Expected output: "static method has been called."
