eddbc45155bba44c010ec6f9dc8a6338aa3caf7f
mayanknaruka <mayanknaruka47@gmail.com>
2026-09-10 09:48:26 +0530

    total integer solved

---
+++ exercise/totalInteger.js
@@ -0,0 +1,21 @@
+// The extra null check is required since typeof null === "object" evaluates to true
+const isObject = (value) => typeof value === 'object' && value !== null;
+
+const totalIntegers = function (obj) {
+  let count = 0;
+
+  if (!isObject(obj)) {
+    return;
+  }
+
+  const elements = Object.values(obj);
+
+  for (const el of elements) {
+    if (Number.isInteger(el)) {
+      count++;
+    } else if (isObject(el)) {
+      count += totalIntegers(el);
+    }
+  }
+  return count;
+};
