e2e2cdf709c775e1e49f62a8e21eb1a738e1b24a
mayanknaruka <mayanknaruka47@gmail.com>
2026-09-10 10:37:36 +0530

    pascal solved but difficult

---
+++ exercise/pascal.js
@@ -0,0 +1,18 @@
+const pascal = function (rowNumber) {
+    if (rowNumber === 1) {
+        return [1];
+    }
+
+    const previousRow = pascal(rowNumber - 1);
+    const previousRowWithZeros = [0, ...previousRow, 0];
+
+    const newRow = [];
+
+    for (let i = 0; i < previousRowWithZeros.length - 1; i += 1) {
+        const leftParent = previousRowWithZeros[i];
+        const rightParent = previousRowWithZeros[i + 1];
+        newRow.push(leftParent + rightParent);
+    }
+
+    return newRow;
+};
