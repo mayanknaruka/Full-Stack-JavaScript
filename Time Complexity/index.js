6e46dc5b4fad2935f8af92391d1885cd3a91caca
mayanknaruka <mayanknaruka47@gmail.com>
2026-09-11 23:49:25 +0530

    time

---
+++ Time Complexity/index.js
@@ -0,0 +1,11 @@
+function oddNumbers(maxNumber) {
+  let currentNumber = 1;
+
+  while (currentNumber < maxNumber) {
+    if (currentNumber % 2 !== 0) {
+      console.log(currentNumber);
+    }
+
+    currentNumber += 1;
+  }
+}
